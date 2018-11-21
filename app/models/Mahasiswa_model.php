e5df872620ce1c1697e9fce2b2fdbae9dcd42564
Irfan-Daimori <daimori1995@gmail.com>
2018-11-21 21:44:29 +0700

    versi 0.0.1

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,107 @@
+<?php 
+
+class Mahasiswa_model {
+	// private $mhs = [
+	// 	[
+	// 		"nama" => "Irfan Daimori",
+	// 		"nim" => "140155201003",
+	// 		"email" => "daimori1995@gmail.com",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	],
+	// 	[
+	// 		"nama" => "Indri Junanda",
+	// 		"nim" => "140155201002",
+	// 		"email" => "indri@gmail.com",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	],
+	// 	[
+	// 		"nama" => "Aris Kurniawan",
+	// 		"nim" => "140155201067",
+	// 		"email" => "aris@gmail.com",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	]
+	// ];
+
+	// private $dbh; // database handler
+	// private $stmt;
+
+	private $table = 'mahasiswa';
+	private $db;
+
+	public function __construct(){
+		$this->db = new Database;
+	}
+
+	public function getAllMahasiswa() {
+		// $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+		// $this->stmt->execute();
+		// return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		$this->db->query('SELECT * FROM ' . $this->table);
+		return $this->db->resultSet();
+
+	}
+
+	public function getMahasiswaById($id) {
+		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+		$this->db->bind('id', $id);
+		return $this->db->single();
+	}
+
+	public function tambahDataMahasiswa($data) {
+		$query = "INSERT INTO mahasiswa
+					VALUES 
+					('', :nama, :nim, :email, :jurusan)";
+		$this->db->query($query);
+		$this->db->bind('nama', $data['nama']);
+		$this->db->bind('nim', $data['nim']);
+		$this->db->bind('email', $data['email']);
+		$this->db->bind('jurusan', $data['jurusan']);
+
+		$this->db->execute();
+
+		return $this->db->rowCount();
+
+	}
+
+	public function hapusDataMahasiswa($id) {
+		$query = "DELETE FROM mahasiswa WHERE id = :id";
+		$this->db->query($query);
+		$this->db->bind('id', $id);
+
+		$this->db->execute();
+
+		return $this->db->rowCount();
+	}
+
+
+	public function ubahDataMahasiswa($data) {
+		$query = "UPDATE mahasiswa SET
+					nama = :nama,	
+					nim = :nim,
+					email = :email,
+					jurusan = :jurusan
+				  WHERE id = :id";
+		$this->db->query($query);
+		$this->db->bind('nama', $data['nama']);
+		$this->db->bind('nim', $data['nim']);
+		$this->db->bind('email', $data['email']);
+		$this->db->bind('jurusan', $data['jurusan']);
+		$this->db->bind('id', $data['id']);
+
+
+		$this->db->execute();
+
+		return $this->db->rowCount();
+
+	}
+
+	public function cariDataMahasiswa(
+	) {
+		$keyword = $_POST['keyword'];
+		$query = "SELECT * FROM mahasiswa WHERE nama LIKE :keyword";
+		$this->db->query($query);
+		$this->db->bind('keyword', "%$keyword%");
+		return $this->db->resultSet();
+	}
+
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+		<div class="jumbotron mt-4">
+	  <h1 class="display-4">Selamat Datang di Website Saya</h1>
+	  <p class="lead">Hai, perkenalkan nama saya <?= $data['nama']; ?></p>
+	  <hr class="my-4">
+	  <a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya</a>
+	</div>
+
+</div>
